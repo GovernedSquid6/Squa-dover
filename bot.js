4efffeed052e073cd731ac6fdc2ab6c934e2e6b7
GovernedSquid6 <41247017+GovernedSquid6@users.noreply.github.com>
2018-07-15 01:11:39 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content.includes === 'pong') {
+    if (message.content.includes(pong)) {
     	message.reply('ping');
   	}
 });
